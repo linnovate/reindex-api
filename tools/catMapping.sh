365d74406558579642a5c7e19534cbdbe765cc4f
rivka haiman <rivka@linnovate.net>
2018-02-13 09:54:12 +0200

    mapping

---
+++ tools/catMapping.sh
@@ -0,0 +1,156 @@
+curl -XPUT localhost:9200/reindex-categories -d ' {
+    "mappings": {
+        "A": {
+             "_parent": {
+                "type": "Z"
+            },
+            "dynamic_templates": [{
+                "cat": {
+                    "match": "*",
+                    "match_mapping_type": "string",
+                    "mapping": {
+                        "type": "string",
+                        "analyzer": "hebrew",
+                        "fields": {
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "he": {
+                                "type": "string",
+                                "analyzer": "hebrew"
+                            },
+                            "ac": {
+                                "type": "string",
+                                "analyzer": "autocomplete",
+                                "search_analyzer": "standard"
+                            }
+                        }
+                    }
+                }
+            }]
+        },
+        "B": {
+            "_parent": {
+                "type": "A"
+            },
+            "dynamic_templates": [{
+                "cat": {
+                    "match": "*",
+                    "match_mapping_type": "string",
+                    "mapping": {
+                        "type": "string",
+                        "analyzer": "hebrew",
+                        "_parent": {
+                            "type": "A"
+                        },
+                        "fields": {
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "he": {
+                                "type": "string",
+                                "analyzer": "hebrew"
+                            },
+                            "ac": {
+                                "type": "string",
+                                "analyzer": "autocomplete",
+                                "search_analyzer": "standard"
+                            }
+                        }
+                    }
+                }
+            }]
+        },
+        "C": {
+            "_parent": {
+                "type": "B"
+            },
+            "dynamic_templates": [{
+                "cat": {
+                    "match": "*",
+                    "match_mapping_type": "string",
+                    "mapping": {
+                        "type": "string",
+                        "analyzer": "hebrew",
+                        "_parent": {
+                            "type": "B"
+                        },
+                        "fields": {
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "he": {
+                                "type": "string",
+                                "analyzer": "hebrew"
+                            },
+                            "ac": {
+                                "type": "string",
+                                "analyzer": "autocomplete",
+                                "search_analyzer": "standard"
+                            }
+                        }
+                    }
+                }
+            }]
+        },
+        "D": {
+            "_parent": {
+                "type": "C"
+            },
+            "dynamic_templates": [{
+                "cat": {
+                    "match": "*",
+                    "match_mapping_type": "string",
+                    "mapping": {
+                        "type": "string",
+                        "analyzer": "hebrew",
+                        "_parent": {
+                            "type": "C"
+                        },
+                        "fields": {
+                            "raw": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            },
+                            "he": {
+                                "type": "string",
+                                "analyzer": "hebrew"
+                            },
+                            "ac": {
+                                "type": "string",
+                                "analyzer": "autocomplete",
+                                "search_analyzer": "standard"
+                            }
+                        }
+                    }
+                }
+            }]
+        }
+    },
+    "settings": {
+        "number_of_shards": 1,
+        "analysis": {
+            "filter": {
+                "autocomplete_filter": {
+                    "type": "edge_ngram",
+                    "min_gram": 2,
+                    "max_gram": 20
+                }
+            },
+            "analyzer": {
+                "autocomplete": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "autocomplete_filter"
+                    ]
+                }
+            }
+        }
+    }
+}
+'
